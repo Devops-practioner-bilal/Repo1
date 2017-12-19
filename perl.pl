770670e1526d5a82ea726d3085f506f50501ddb8
Devops-practioner-bilal <bilalnetbuzz@gmail.com>
2017-12-19 19:38:43 +0530

    perl file

---
+++ perl.pl
@@ -0,0 +1,21 @@
+#! /usr/bin/perl.pl
+
+
+$a = 10;
+$b = 0;
+
+#print "$a\n";
+
+while ($a > $b) {
+$b++;
+print "Count $b: $A is equal to 10\n";
+#exit();
+}
+
+@d = qw{BIlal sana ammi shaheen};
+$c = "BIlal sana ammi shaheen";
+#$e = @d;
+$e = $#d;
+print "$c\n";
+print "@d\n";
+print "$e\n";
